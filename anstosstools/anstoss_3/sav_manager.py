41d1cdffaa2ab73a6cb10e6d8b78b51b8f6d3da5
eric <48635802+ericziethen@users.noreply.github.com>
2021-02-14 15:21:44 +1100

    File convertion framework

---
+++ anstosstools/anstoss_3/sav_manager.py
@@ -0,0 +1,14 @@
+
+SUPPORTED_FILES = [
+
+]
+
+SAV_ENCODING = 'windows-1252'
+
+
+def convert_sav_dir_to_json(*, sav_dir, json_dir):
+    pass
+
+
+def convert_json_dir_to_sav(*, json_dir, sav_dir):
+    pass
--- tests/anstosstools/integration/Anstoss3/v1.40/test_convert_sav_data.py
+++ tests/anstosstools/integration/Anstoss3/v1.40/test_convert_sav_data.py
@@ -1,19 +1,41 @@
 
-from anstosstools.data_manager import convert_sav_dir_to_json
+import os
+
+from anstosstools.anstoss_3.sav_manager import (
+    SUPPORTED_FILES, SAV_ENCODING,
+    convert_sav_dir_to_json, convert_json_dir_to_sav
+)
+
+ANSTOSS_3_SAV_INPUT_DIR = 'tests/anstosstools/integration/TestFiles/Anstoss3/v1.40'
 
 
+def test_import_export_test_dir(tmpdir):
 
-# def test_import_export_test_dir(tmpdir):
+    # Setup test dirs
+    json_dir = os.path.join(tmpdir, 'json')
+    os.mkdir(json_dir)
+    sav_out_dir = os.path.join(tmpdir, 'sav_out')
+    os.mkdir(sav_out_dir)
 
+    # Convert to json
+    convert_sav_dir_to_json(sav_dir=ANSTOSS_3_SAV_INPUT_DIR, json_dir=json_dir)
 
-# def test_import_export_to_from_json(tmpdir):
+    # convert back to sav
+    convert_json_dir_to_sav(json_dir=json_dir, sav_dir=sav_out_dir)
 
+    # compare supported files
+    for file_name in SUPPORTED_FILES:
+        in_file_path = os.path.join(ANSTOSS_3_SAV_INPUT_DIR, file_name)
+        convert_file_path = os.path.join(sav_out_dir, file_name)
 
-# D:\# Eric Projects\anstosstools\tests\anstosstools\integration\TestFiles\Anstoss3\v1.40\ExSpiel.sav
+        assert os.path.exists(convert_file_path)
 
-def test_fail():
-    # FAIL UNTIL INTEGRATION TEST ADDED
-    assert False
+        with open(in_file_path, 'r', encoding=SAV_ENCODING) as in_file_ptr:
+            with open(convert_file_path, 'r', encoding=SAV_ENCODING) as convert_file_ptr:
+                in_file_list = list(in_file_ptr)
+                convert_file_list = list(convert_file_ptr)
 
+                assert len(in_file_list) == len(convert_file_list)
 
-
+                for idx, line in enumerate(in_file_list):
+                    assert line == convert_file_list[idx]
